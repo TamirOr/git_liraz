694a151e2b397d1b6c678e5adf14aef795948a46
tamir.or <tamir.or@riskified.com>
2022-03-20 20:20:50 +0200

    same brnach, new commit!

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-void func_to_git(const char* name);
+int func_to_git(const char* name);
 
 int main() {
     printf("Hello, World!\n");
     return 0;
 }
 
-void func_to_git(const char* name) {
+int func_to_git(const char* name) {
     printf("%S", name);
 }
